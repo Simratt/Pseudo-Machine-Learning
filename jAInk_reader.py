17c6f9f1367332699b77b9210bdb571c07d3e475
Simratt <47543274+Simratt@users.noreply.github.com>
2022-06-22 19:03:10 -0400

    Code genereates text from inputted stories

---
+++ jAInk_reader.py
@@ -0,0 +1,60 @@
+import random
+
+def prep_dataset(addr: str) -> dict:
+    ### VARIABLES ###
+
+    text = []
+    wordlist = []
+    chars = "\"\',?!.-"
+    dataset = {'':[]}
+    count = 0
+    safety = 1
+    # Loading the text file
+    with open(addr) as t:
+        text = t.readlines()
+        t.close()
+
+    # Extracting every word from the text and formatting it 
+    for i in range(len(text)):
+        wordlist.extend(text[i][4:-2].split())
+    
+    # this is if I want to take out any punctuation
+    # for p in range(len(wordlist)):
+    #     for character in chars:
+    #         if character in wordlist[p]:
+    #             wordlist[p] = wordlist[p].replace(character, '')
+
+
+    # Make the dataset
+    for w in range(len(wordlist)):
+        if wordlist[w] in dataset and safety < (len(wordlist)-2):
+            dataset[wordlist[w]].append(wordlist[safety])
+        else:
+            if safety < (len(wordlist)-2):
+                dataset[wordlist[w]] = [wordlist[safety]]
+        safety +=1
+    
+    return dataset
+
+def randomize(dataset: dict) -> str:
+    dkeys = list(dataset.keys())
+    key = dkeys[random.randint(0, len(dkeys)-1)]
+    value = dataset[key][random.randint(0, len(dataset[key])-1)]
+    paragraph = ''
+    count = 0
+    while count <= 50:
+        paragraph += " " + value
+        key = value
+        value = dataset[key][random.randint(0, len(dataset[key])-1)]
+        count += 1
+    paragraph += "..."
+    return(paragraph)
+
+def switch(key, value):
+    key = value
+    return key
+
+if __name__ == '__main__':
+    set = prep_dataset('/Users/simratb/Projects/Python/shakespeare.txt')
+    story = randomize(set)
+    print(story)
